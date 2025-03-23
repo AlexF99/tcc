a64f49f5baee5dfb7280931822d2fc1f6e67b813
Lewis <lgtg20@inf.ufpr.br>
2025-03-23 13:19:48 -0300

    remove unused classes and interfaces; delete log4j configuration files and Python script

--- ecp/src/main/java/br/edu/utfpr/pena/fdcd/algorithms/enumeration/DCEnumeration.java
+++
@@ -1,8 +0,0 @@
-package br.edu.utfpr.pena.fdcd.algorithms.enumeration;
-
-import java.util.BitSet;
-import java.util.Set;
-
-public interface DCEnumeration {
-	public Set<BitSet> searchDCs();
-}
--- src/metrics/main.py
+++ src/metrics/main.py
@@ -7,7 +7,7 @@ import os
 from datetime import datetime
 
 dataset_csv_file = "/home/lewis/ufpr/tcc/datasets/ncvoter_1001r_19c.csv"
-fds_input_file = "/home/lewis/ufpr/tcc/metanome-cli/results/2025-03-14_11-36-24_fds"
+fds_input_file = "/home/lewis/ufpr/tcc/metanome-cli/results/2025-03-23_12-49-55_fds"
 
 current_time = datetime.now().strftime("%d-%m-%Y-%H-%M-%S")
 output_folder = os.path.join("metrics_results", current_time)
@@ -27,50 +27,28 @@ total_time_metrics = {metric_name: 0 for metric_name in metrics}
 results = parser.load_results(fds_input_file)
 fds = parser.metanome_parser(results)
 
-
-
 df = pd.read_csv(dataset_csv_file, header="infer")
 
-print(df.columns)
-
-
 num_columns = df.shape[1]
-# df.columns = [f"column{i+1}" for i in range(num_columns)]
+df.columns = [f"column{i+1}" for i in range(num_columns)]
 
 for lhs_columns, rhs_column in fds:
-
     fd_identifier = f"{lhs_columns}->{rhs_column}"
     
-    # print(lhs_columns)
-    # print(df.columns)
-    
-    # exit()
-    
-    # if not set(lhs_columns).issubset(df.columns) or rhs_column not in df.columns:
-    #     print(f"Columns {lhs_columns} or {rhs_column} not found in {dataset_csv_file}")
-    #     continue
+    if not set(lhs_columns).issubset(df.columns) or rhs_column not in df.columns:
+        print(f"Columns {lhs_columns} or {rhs_column} not found in {dataset_csv_file}")
+        continue
+
     
     for metric_name, metric_function in metrics.items():
         start_time = time.time()
         try:
             if not lhs_columns:
                 print("LHS está vazio")
-                continue
-            
-            result = metric_function(df, lhs_columns, rhs_column)
-            
+                continue            
 
-            # if metric_function in paper_metrics.__dict__.values():
-            #     if len(lhs_columns) != 1:
-            #         print(f"[WARN] Métrica '{metric_name}' só aceita um argumento em LHS atual: {lhs_columns}. Ignorando esta FD.")
-            #         continue
-                
-            #     lhs_column = lhs_columns[0]
-            #     print(f"Calculando {metric_name} para {lhs_column} -> {rhs_column} no arquivo {dataset_csv_file}")
-            #     result = metric_function(df, lhs_column, rhs_column) 
-            # else:
-            #     print(f"Calculando {metric_name} para {lhs_columns} -> {rhs_column} no arquivo {dataset_csv_file}")
-            #     result = metric_function(df, lhs_columns, rhs_column)
+            print(f"Calculando {metric_name} para {lhs_columns} -> {rhs_column} no arquivo {dataset_csv_file}")
+            result = metric_function(df, lhs_columns, rhs_column)
 
             elapsed_time = time.time() - start_time
             total_time_metrics[metric_name] += elapsed_time
@@ -88,7 +66,6 @@ for lhs_columns, rhs_column in fds:
             continue
 
 total_time_file = time.time() - start_time_file
-print("PASSSEIII")
 for metric_name, total_time in total_time_metrics.items():
     pd.DataFrame([{
         "file": dataset_csv_file,
