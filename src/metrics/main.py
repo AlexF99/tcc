d0f3c2f9a3224e211f1254e334d6f7904d67ed7a
AlexF99 <alexandreopf@gmail.com>
2025-04-05 16:19:55 -0300

    fixed header issue, we can run metrics for fdx now

--- src/metrics/main.py
+++ src/metrics/main.py
@@ -8,20 +8,25 @@ import argparse
 
 # Set up argument parser
 parser = argparse.ArgumentParser(description="Run metrics on datasets")
-parser.add_argument("--dataset", type=str, required=True, help="Path to the dataset CSV file")
+parser.add_argument(
+    "--dataset", type=str, required=True, help="Path to the dataset CSV file"
+)
 parser.add_argument("--fds", type=str, required=True, help="Path to the FDs input file")
-parser.add_argument("--type", type=str, required=True, help="Metanome or FDX")
+parser.add_argument("--type", type=str, required=True, help="metanome or fdx")
+parser.add_argument(
+    "--header", type=str, required=True, help="input data has header as first row"
+)
 parser.add_argument("--output", type=str, required=False, help="Output's dir name")
 
 args = parser.parse_args()
 
-print(args.dataset, args.fds)
 dataset_csv_file = args.dataset
 fds_input_file = args.fds
+hasHeader = args.header == "True"
 source_type = args.type
 
 dir_name = datetime.now().strftime("%d-%m-%Y-%H-%M-%S")
-if (args.output):
+if args.output:
     dir_name = args.output
 output_folder = os.path.join("metrics_results", dir_name)
 os.makedirs(output_folder, exist_ok=True)
@@ -48,14 +53,21 @@ fds = (
     else metrics_parser.fdx_parser(results)
 )
 
-df = pd.read_csv(dataset_csv_file, header="infer")
+if hasHeader:
+    df = pd.read_csv(dataset_csv_file, header=0)  # Use first row as column names
+else:
+    df = pd.read_csv(dataset_csv_file, header=None)  # No header, treat all as data
+    num_columns = df.shape[1]
+    df.columns = [f"column{i+1}" for i in range(num_columns)]
 
 num_columns = df.shape[1]
-df.columns = [f"column{i+1}" for i in range(num_columns)]
 
 for lhs_columns, rhs_column in fds:
+
+    lhs_columns = [col.strip() for col in lhs_columns]
+    rhs_column = rhs_column.strip()
     fd_identifier = f"{lhs_columns}->{rhs_column}"
-    
+
     if not set(lhs_columns).issubset(df.columns) or rhs_column not in df.columns:
         print(f"Columns {lhs_columns} or {rhs_column} not found in {dataset_csv_file}")
         continue
@@ -65,40 +77,56 @@ for lhs_columns, rhs_column in fds:
         try:
             if not lhs_columns:
                 print("LHS está vazio")
-                continue            
+                continue
 
-            print(f"Calculando {metric_name} para {lhs_columns} -> {rhs_column} no arquivo {dataset_csv_file}")
+            print(
+                f"Calculando {metric_name} para {lhs_columns} -> {rhs_column} no arquivo {dataset_csv_file}"
+            )
             result = metric_function(df, lhs_columns, rhs_column)
 
             elapsed_time = time.time() - start_time
             total_time_metrics[metric_name] += elapsed_time
-            
-            pd.DataFrame([{
-                "file": dataset_csv_file,
-                "fd": fd_identifier,
-                "metric": metric_name,
-                "metric_result_fd": result,
-                "metric_time_fd": elapsed_time
-            }]).to_csv(fds_csv_file, mode='a', header=False, index=False)
-        
+
+            pd.DataFrame(
+                [
+                    {
+                        "file": dataset_csv_file,
+                        "fd": fd_identifier,
+                        "metric": metric_name,
+                        "metric_result_fd": result,
+                        "metric_time_fd": elapsed_time,
+                    }
+                ]
+            ).to_csv(fds_csv_file, mode="a", header=False, index=False)
+
         except Exception as e:
-            print(f"[ERROR] {metric_name} for {lhs_columns} and {rhs_column} in {dataset_csv_file}: {e}")
+            print(
+                f"[ERROR] {metric_name} for {lhs_columns} and {rhs_column} in {dataset_csv_file}: {e}"
+            )
             continue
 
 total_time_file = time.time() - start_time_file
 for metric_name, total_time in total_time_metrics.items():
-    pd.DataFrame([{
-        "file": dataset_csv_file,
-        "metric": metric_name,
-        "metric_time": total_time,
-        "num_columns": num_columns,
-        "num_fds": len(results)
-    }]).to_csv(metrics_csv_file, mode='a', header=False, index=False)
+    pd.DataFrame(
+        [
+            {
+                "file": dataset_csv_file,
+                "metric": metric_name,
+                "metric_time": total_time,
+                "num_columns": num_columns,
+                "num_fds": len(results),
+            }
+        ]
+    ).to_csv(metrics_csv_file, mode="a", header=False, index=False)
 
-pd.DataFrame([{
-    "file": dataset_csv_file,
-    "metric": "total_file_time",
-    "metric_time": total_time_file,
-    "num_columns": num_columns,
-    "num_fds": len(results)
-}]).to_csv(metrics_csv_file, mode='a', header=False, index=False)
+pd.DataFrame(
+    [
+        {
+            "file": dataset_csv_file,
+            "metric": "total_file_time",
+            "metric_time": total_time_file,
+            "num_columns": num_columns,
+            "num_fds": len(results),
+        }
+    ]
+).to_csv(metrics_csv_file, mode="a", header=False, index=False)
--- src/metrics/run.sh
+++ src/metrics/run.sh
@@ -5,7 +5,7 @@ RESULTS_DIR="../../metanome-cli/results"
 DATASETS_DIR="../../datasets"
 SCRIPT="main.py"
 
-# Loop through each FD file in the results directory
+# metrics for metanome results
 for fd_file in "$RESULTS_DIR"/*; do
   # Extract the algorithm and dataset names from the FD filename
   filename=$(basename -- "$fd_file")
@@ -13,23 +13,20 @@ for fd_file in "$RESULTS_DIR"/*; do
   dataset="${filename#*-}"
   type="metanome"
 
-  # Construct the path to the dataset file
   dataset_file="$DATASETS_DIR/$dataset"
 
   # Check if the dataset file exists
   if [ -f "$dataset_file" ]; then
-    # Construct the output directory name
     output_dir="${algorithm}-${dataset}"
 
-    # Run the main.py script with the appropriate arguments
     echo "Running main.py for dataset: $dataset_file and FDs: $fd_file"
-    python3 "$SCRIPT" --dataset "$dataset_file" --fds "$fd_file" --output "$output_dir" --type "$type"
+    python3 "$SCRIPT" --dataset "$dataset_file" --fds "$fd_file" --output "$output_dir" --type "$type" --header "False"
   else
     echo "Dataset file not found: $dataset_file"
   fi
 done
 
-# todo: make this work with headers
+# metrics for fdx results
 for fd_file in ../../profiler/results/*; do
   algorithm="fdx"
   dataset_name="$(basename ${fd_file%_by_col.txt})"
@@ -37,13 +34,12 @@ for fd_file in ../../profiler/results/*; do
 
   dataset_file="$DATASETS_DIR/$dataset_name.csv"
 
+  # Check if the dataset file exists
   if [ -f "$dataset_file" ]; then
-    # Construct the output directory name
-    output_dir="${algorithm}-${dataset}"
+    output_dir="${algorithm}-${dataset_name}"
 
-    # Run the main.py script with the appropriate arguments
     echo "Running main.py for dataset: $dataset_file and FDs: $fd_file"
-    python3 "$SCRIPT" --dataset "$dataset_file" --fds "$fd_file" --output "$output_dir" --type "$type"
+    python3 "$SCRIPT" --dataset "$dataset_file" --fds "$fd_file" --output "$output_dir" --type "$type" --header "True"
   else
     echo "Dataset file not found: $dataset_file"
   fi
