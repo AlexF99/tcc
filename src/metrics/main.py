7577c10c395076ad6896094839d4f9a77b9cc25c
AlexF99 <alexandreopf@gmail.com>
2025-04-05 16:57:02 -0300

    improved results csv output format

--- src/metrics/main.py
+++ src/metrics/main.py
@@ -62,11 +62,16 @@ else:
 
 num_columns = df.shape[1]
 
+fd_ids = []
+metrics_results = {key: [] for key in metrics}
+
 for lhs_columns, rhs_column in fds:
 
     lhs_columns = [col.strip() for col in lhs_columns]
     rhs_column = rhs_column.strip()
     fd_identifier = f"{lhs_columns}->{rhs_column}"
+
+    fd_ids.append(fd_identifier)
 
     if not set(lhs_columns).issubset(df.columns) or rhs_column not in df.columns:
         print(f"Columns {lhs_columns} or {rhs_column} not found in {dataset_csv_file}")
@@ -84,26 +89,22 @@ for lhs_columns, rhs_column in fds:
             )
             result = metric_function(df, lhs_columns, rhs_column)
 
+            metrics_results[metric_name].append(result)
+
             elapsed_time = time.time() - start_time
             total_time_metrics[metric_name] += elapsed_time
-
-            pd.DataFrame(
-                [
-                    {
-                        "file": dataset_csv_file,
-                        "fd": fd_identifier,
-                        "metric": metric_name,
-                        "metric_result_fd": result,
-                        "metric_time_fd": elapsed_time,
-                    }
-                ]
-            ).to_csv(fds_csv_file, mode="a", header=False, index=False)
 
         except Exception as e:
             print(
                 f"[ERROR] {metric_name} for {lhs_columns} and {rhs_column} in {dataset_csv_file}: {e}"
             )
             continue
+
+final_df = pd.DataFrame(
+    {"fd": fd_ids, "mu": metrics_results["mu"], "rfi": metrics_results["rfi"]}
+)
+
+final_df.to_csv(fds_csv_file, mode="w", header=True, index=False)
 
 total_time_file = time.time() - start_time_file
 for metric_name, total_time in total_time_metrics.items():
